e6a700faf81bd7ea6fb4c22506094648ddde164d
McKenna Wright <wrightmc@oregonstate.edu>
2018-11-17 10:51:45 -0800

    Added console log request

--- main.js
+++ main.js
@@ -4,9 +4,27 @@ $(updateView)
 
 function updateView() {
   $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
+  $.getJSON(BASE_URL + "/rides/count/per_month")
+  var ctx= document.getElementById ('myChart').getContext('2d');
+
+  var chart = new Chart(ctx, {
+    type: line 
+
+    data: {
+      labels: ("total rides")
+      datasets: [{
+        labels: "Zagster Rides"
+        backgroundColor: '#3f3f3f'
+        data: }],
+    }
+  })
 }
 
 function updateRideCount(data) {
   numberOfRides = data.count
   $("h2#rideCount").html(numberOfRides)
+  console.log(data)
+
+  function updatePerMonth(data)
+  console.log(data)
 }
