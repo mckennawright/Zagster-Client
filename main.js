d8464be88bcdb583d86880e305ad499ad653fd2e
McKenna Wright <wrightmc@oregonstate.edu>
2018-11-17 17:14:09 -0800

    updates to main.js

--- main.js
+++ main.js
@@ -1,31 +1,36 @@
-const BASE_URL = "https://zagster-service.herokuapp.com"
+onst BASE_URL = "https://zagster-service.herokuapp.com"
 
 $(updateView)
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
 
 function updateView() {
-  $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
-  $.getJSON(BASE_URL + "/rides/count/per_month")
-  var ctx= document.getElementById ('myChart').getContext('2d');
+    $.getJSON(BASE_URL + "/rides/count", updateRideCount)
+    $.getJSON(BASE_URL + "/rides/count/per_month", updatePerMonth)
 
-  var chart = new Chart(ctx, {
-    type: line 
-
-    data: {
-      labels: ("total rides")
-      datasets: [{
-        labels: "Zagster Rides"
-        backgroundColor: '#3f3f3f'
-        data: }],
-    }
-  })
 }
 
 function updateRideCount(data) {
-  numberOfRides = data.count
-  $("h2#rideCount").html(numberOfRides)
-  console.log(data)
+    numberOfRides = data.count
+    $("h2#rideCount").html(numberOfRides)
+     console.log(data)
+}
 
-  function updatePerMonth(data)
-  console.log(data)
+function updatePerMonth(data) {
+    console.log(data[2016][0])
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'bar',
+    
+      
+        data: {
+            labels: ["total rides"],
+            datasets: [{
+                label: "Zagster Rides",
+                backgroundColor: '#3f3f3f',
+                borderColor: '#FC4A1A',
+                data: [data[2016][0][9],data[2016][1][10],data[2016][2][11]],
+            }]
+        },
+        options: {}
+    });
 }
