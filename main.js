0d4d13eca36ea5f8b9e557e383ed69d514e9d326
McKenna Wright <wrightmc@oregonstate.edu>
2018-11-18 18:06:22 -0800

    Chart..

--- main.js
+++ main.js
@@ -18,7 +18,6 @@ function updatePerMonth(data) {
     console.log(data[2016][0])
     var ctx = document.getElementById('myChart').getContext('2d');
     var chart = new Chart(ctx, {
-        // The type of chart we want to create
         type: 'bar',
     
       
