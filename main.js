22d406dfa7dcc4342bfcf09565c82df673ae67b8
McKenna Wright <wrightmc@oregonstate.edu>
2018-11-25 16:08:27 -0800

    Trying to fix data

--- main.js
+++ main.js
@@ -8,9 +8,10 @@ function updateView() {
     $.getJSON(BASE_URL + "/rides/count", updateRideCount)
     $.getJSON(BASE_URL + "/rides/count/per_month", updatePerMonth)
 }
-function getData(data){
- rideData = data;
-console.log(rideData);
-
- for (var i = 0; i <=12; ++i){
-    chartData.push(rideData[2017] [i] [i+1]); 
+function getData(data) {
+    rideData = data;
+    console.log(rideData);
+} 
+for ( var i = [0]; i <= [11]; ++i){
+chartData.push(rideData[2017] [i] [i+1]);
+}
